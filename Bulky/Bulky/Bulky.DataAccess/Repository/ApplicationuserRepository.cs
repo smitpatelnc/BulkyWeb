3f3478f69bbfedbbdd3ef44a542e2fa7bc1eeebf
smitpatelnc <158784114+smitpatelnc@users.noreply.github.com>
2024-12-12 02:31:15 -0500

    xfv
    sfdv

---
+++ Bulky/Bulky/Bulky.DataAccess/Repository/ApplicationuserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ApplicationuserRepository : Repository<ApplicationUser>, IApplicationuserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationuserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
