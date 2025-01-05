664b907fa6c45bc0bab4ca2fb15af705df587f63
smitpatelnc <158784114+smitpatelnc@users.noreply.github.com>
2025-01-05 14:23:19 -0500

    yes

--- Bulky/Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec50407a042cd5aaf4f888fb9f89047c03db550d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c13545ee0af6e93f254cdb9bc6ed6208c6da554")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
