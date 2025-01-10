903e2369c167c195c1546a76f5bc75faebe7600c
smitpatelnc <158784114+smitpatelnc@users.noreply.github.com>
2025-01-10 15:05:44 -0500

    cm
    cm

--- Bulky/Bulky/BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ Bulky/Bulky/BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -27,29 +27,28 @@ namespace BulkyWeb.Areas.Customer.Controllers
         }
         public IActionResult Index()
         {
-
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
             ShoppingCartVM = new()
             {
                 ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
-               includeProperties: "Product"),
-				OrderHeader = new()
+                includeProperties: "Product"),
+                OrderHeader = new()
             };
+            IEnumerable<ProductImage> productImages = _unitOfWork.ProductImage.GetAll();
 
             foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
+                cart.Product.ProductImages = productImages.Where(u => u.ProductId == cart.Product.Id).ToList();
                 cart.Price = GetPriceBasedOnQuantity(cart);
                 ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
             }
 
-
             return View(ShoppingCartVM);
         }
 
-
-		public IActionResult Summary()
+        public IActionResult Summary()
 		{
 			var claimsIdentity = (ClaimsIdentity)User.Identity;
 			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
