f4ac73a7c65dcf52a293830830e8cbcbf7067c18
smitpatelnc <158784114+smitpatelnc@users.noreply.github.com>
2025-01-10 15:04:03 -0500

    cmq
    cm

--- Bulky/Bulky/Bulky.DataAccess/Migrations/20250106233604_InitialMigration.Designer.cs
+++ Bulky/Bulky/Bulky.DataAccess/Migrations/20250106233604_InitialMigration.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bulky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241218010930_addOrderheaderAndOrderdetails")]
-    partial class addOrderheaderAndOrderdetails
+    [Migration("20250106233604_InitialMigration")]
+    partial class InitialMigration
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -274,6 +274,9 @@ namespace Bulky.DataAccess.Migrations
                     b.Property<double>("Price50")
                         .HasColumnType("float");
 
+                    b.Property<int>("TestProduct")
+                        .HasColumnType("int");
+
                     b.Property<string>("Title")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
@@ -297,6 +300,7 @@ namespace Bulky.DataAccess.Migrations
                             Price = 90.0,
                             Price100 = 80.0,
                             Price50 = 85.0,
+                            TestProduct = 0,
                             Title = "Fortune of Time"
                         },
                         new
@@ -311,6 +315,7 @@ namespace Bulky.DataAccess.Migrations
                             Price = 30.0,
                             Price100 = 20.0,
                             Price50 = 25.0,
+                            TestProduct = 0,
                             Title = "Dark Skies"
                         },
                         new
@@ -325,6 +330,7 @@ namespace Bulky.DataAccess.Migrations
                             Price = 50.0,
                             Price100 = 35.0,
                             Price50 = 40.0,
+                            TestProduct = 0,
                             Title = "Vanish in the Sunset"
                         },
                         new
@@ -339,6 +345,7 @@ namespace Bulky.DataAccess.Migrations
                             Price = 65.0,
                             Price100 = 55.0,
                             Price50 = 60.0,
+                            TestProduct = 0,
                             Title = "Cotton Candy"
                         },
                         new
@@ -353,6 +360,7 @@ namespace Bulky.DataAccess.Migrations
                             Price = 27.0,
                             Price100 = 20.0,
                             Price50 = 25.0,
+                            TestProduct = 0,
                             Title = "Rock in the Ocean"
                         },
                         new
@@ -367,6 +375,7 @@ namespace Bulky.DataAccess.Migrations
                             Price = 23.0,
                             Price100 = 20.0,
                             Price50 = 22.0,
+                            TestProduct = 0,
                             Title = "Leaves and Wonders"
                         });
                 });
